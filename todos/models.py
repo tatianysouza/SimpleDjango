7afc3dc389ba1479be2412759b34e89244294693
LopesLs <97265556+LopesLs@users.noreply.github.com>
2024-01-05 08:45:10 -0300

    (refactor): mudando alguns fields
    Percebe-se que alguns fields do model são reduntantes e que por padrão os valores null e blank já são como false.

--- todos/models.py
+++ todos/models.py
@@ -3,11 +3,9 @@ from django.db import models
 
 
 class Todo(models.Model):
-    title = models.CharField(
-        verbose_name="Título", max_length=100, null=False, blank=False
-    )
-    created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
-    deadline = models.DateField(verbose_name="Data limite", null=False, blank=False)
+    title = models.CharField(verbose_name="Título", max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateField(verbose_name="Data limite")
     finished_at = models.DateField(null=True)
 
     class Meta:
